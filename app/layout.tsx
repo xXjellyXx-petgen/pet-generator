8f04a294524bb60b973b69caf5beeb429f383155
Tanga <seanjacobolivas21@gmail.com>
2025-08-14 18:54:27 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Pet generator',
+  description: 'Created by Kairu',
+  generator: 'made by Kairu',
 }
 
 export default function RootLayout({
